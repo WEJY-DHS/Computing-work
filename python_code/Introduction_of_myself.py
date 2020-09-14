f7c84ef6b70e661dec56f3066f61c71c18fc5ba6
WEJY-DHS <69775755+WEJY-DHS@users.noreply.github.com>
2020-09-14 09:19:22 +0800

    Create Introduction_of_myself.py

---
+++ python_code/Introduction_of_myself.py
@@ -0,0 +1,8 @@
+#This code should scroll the words 'Hello, my name is Wynne!' across the micro:bit
+#Unfortunately there is an error :(
+#Can you fix it?
+
+
+
+name = input("What is your name?")
+print(Hello, my name is Wynne )
